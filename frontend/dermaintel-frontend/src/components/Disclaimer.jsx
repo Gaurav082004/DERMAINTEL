36e11f5cbb42006d20c80469107ed3d904402ef5
Gaurav082004 <gauravkumar082004@gmail.com>
2026-09-02 22:47:48 +0530

    Remove trial frontend

--- frontend/dermaintel-frontend/src/components/Disclaimer.jsx
+++
@@ -1,7 +0,0 @@
-export default function Disclaimer({ children }) {
-  return (
-    <div className="disclaimer">
-      {children || 'This is a research prototype and is not a substitute for professional medical diagnosis.'}
-    </div>
-  );
-}
